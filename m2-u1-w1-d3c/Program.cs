d460a60c2e86e33d7bc1b76a0c9e192108794eb5
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-01-31 16:55:08 +0100

    Aggiungere i file di progetto.

---
+++ m2-u1-w1-d3c/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Inserisci su quanti numeri vuoi lavorare: ");
+        int dimensione = Convert.ToInt32(Console.ReadLine());
+        int[] numeri = new int[dimensione];
+
+
+        int somma = 0;
+        for (int i = 0; i < dimensione; i++)
+        {
+            Console.Write($"Inserisci il numero {i + 1}: ");
+            numeri[i] = Convert.ToInt32(Console.ReadLine());
+            somma += numeri[i];
+        }
+
+        double media = (double)somma / dimensione;
+
+        Console.WriteLine($"La somma di tutti i numeri è: {somma}");
+        Console.WriteLine($"La media aritmetica è: {media}");
+
+        Console.ReadLine();
+    }
+}
